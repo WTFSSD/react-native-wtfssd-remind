e9798c2a9f9727b1a96418cd90175ed8fb2f0c11
莲子 <15502723800@163.com>
2018-01-05 12:13:21 +0800

    first commit

---
+++ ios/RCTRemind/RCTRemind/RCTRemind.h
@@ -0,0 +1,13 @@
+//
+//  RCTRemind.h
+//  RCTRemind
+//
+//  Created by WTFSSD on 2018/1/5.
+//  Copyright © 2018年 marhub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTRemind : NSObject
+
+@end
---
+++ ios/RCTRemind/RCTRemind/RCTRemind.m
@@ -0,0 +1,13 @@
+//
+//  RCTRemind.m
+//  RCTRemind
+//
+//  Created by WTFSSD on 2018/1/5.
+//  Copyright © 2018年 marhub. All rights reserved.
+//
+
+#import "RCTRemind.h"
+
+@implementation RCTRemind
+
+@end
